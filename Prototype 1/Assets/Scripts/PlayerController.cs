2dc810af1a2e1e93c95a4605fecf03dfb221d9b7
akillness38 <31362761+akillness@users.noreply.github.com>
2024-06-09 00:52:53 +0900

    Update PlayerController.cs

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
